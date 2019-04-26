7eaa30355ed783b6dc06cccfe679889df68066b7
DarkiDev <rageskilln@gmail.com>
2019-04-27 01:19:49 +0200

    Adding javadocs to the classes, methods and members

--- server/src/main/java/net/prokyo/codesync/server/session/Session.java
+++ server/src/main/java/net/prokyo/codesync/server/session/Session.java
@@ -8,14 +8,27 @@ import net.prokyo.codesync.server.CSConnection;
 import net.prokyo.codesync.server.team.Member;
 import net.prokyo.codesync.server.team.Permission;
 
+/**
+ * The session stores data about the current project CodeSync syncs like all members connected to the session.
+ * It also is the intersect of the team management with the server.
+ */
 public class Session {
 
+	/**
+	 * All members connected to the session.
+	 */
 	@Getter private List<Member> members;
 
 	public Session() {
 		this.members = new CopyOnWriteArrayList<>();
 	}
 
+	/**
+	 * Adds a member to the members list of this session.
+	 * @param connection The CodeSync-Network-Connection of the member.
+	 * @param isHost     True if this member is the host of the session.
+	 * @return The created and registered member object-
+	 */
 	public Member addMember(CSConnection connection, boolean isHost) {
 		UUID id;
 		while (this.getMember(id = UUID.randomUUID()) != null) ;
@@ -24,14 +37,29 @@ public class Session {
 		return member;
 	}
 
+	/**
+	 * Returns the member by the given name.
+	 * @param name The name of the member to be searching for.
+	 * @return The member object of the given name or null if no member with this name could be found.
+	 */
 	public Member getMember(String name) {
 		return this.members.stream().filter(member -> member.getName().equalsIgnoreCase(name)).findFirst().orElse(null);
 	}
 
+	/**
+	 * Returns the member by the given UUID.
+	 * @param id The UUID of the member to be searching for.
+	 * @return The member object of the given UUID or null if no member could be found.
+	 */
 	public Member getMember(UUID id) {
 		return this.members.stream().filter(member -> member.getId().equals(id)).findFirst().orElse(null);
 	}
 
+	/**
+	 * Checks if the given name is already existing by checking if a member by this name already exists.
+	 * @param name The name to be checked.
+	 * @return True if the name exists.
+	 */
 	public boolean isNameExisting(String name) {
 		return this.getMember(name) != null;
 	}
--- server/src/main/java/net/prokyo/codesync/server/team/Member.java
+++ server/src/main/java/net/prokyo/codesync/server/team/Member.java
@@ -5,11 +5,27 @@ import lombok.Getter;
 import lombok.Setter;
 import net.prokyo.codesync.server.CSConnection;
 
+/**
+ * The member is the object uniting the pure network connection with the CodeSync team management and general project management.
+ * It offers the functionality for permission-handling and subdivision of members by name and UUID.
+ */
 public class Member {
 
+	/**
+	 * The CodeSync-Network-Connection of the member.
+	 */
 	@Getter private CSConnection connection;
+	/**
+	 * The UUID of the member. This ID is - as the abbreviation tells - unique and must not existing.
+	 */
 	@Getter private UUID id;
+	/**
+	 * The name of the member. The name should be unique as well, but does not have to.
+	 */
 	@Getter @Setter private String name;
+	/**
+	 * The permissions shifted into a int value.
+	 */
 	private int permissions;
 
 	public Member(UUID id, CSConnection connection, Permission... permissions) {
@@ -18,14 +34,27 @@ public class Member {
 		this.permissions = Permission.calculate(permissions);
 	}
 
+	/**
+	 * Checks if the member has the given permission.
+	 * @param permission The permission to be checked.
+	 * @return True if the member has the given permission.
+	 */
 	public boolean hasPermission(Permission permission) {
 		return (permission.value() & this.permissions) == permission.value();
 	}
 
+	/**
+	 * Adds a permission to the permissions of the member.
+	 * @param permission The permission to be added.
+	 */
 	public void addPermission(Permission permission) {
 		this.permissions |= permission.value();
 	}
 
+	/**
+	 * Revokes a permission from the permissions of the member.
+	 * @param permission The permission to be revoked.
+	 */
 	public void removePermission(Permission permission) {
 		this.permissions &= ~permission.value();
 	}
